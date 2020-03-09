e58e720785dcde1c8a2659ffa4ac954cddd231cf
sonabk997 <61184554+sonabk997@users.noreply.github.com>
2020-03-09 09:07:48 +0530

    Add files via upload

---
+++ trap.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+float main()
+{
+float area,a,b;
+printf("enter the values of a,b");
+scanf("%f" "%f" ,&a, &b);
+area=(3.14152 *a*b);
+printf("area of eclipse=%f\n",area);
+}
---
+++ value.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+int a=10;
+printf("a value is %d",a);
+printf("a value is %d",++a);
+printf("a value is %d",a);
+}
