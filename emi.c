f998de0bbfc6384f047330debe1750d2e520363c
sonabk997 <61184554+sonabk997@users.noreply.github.com>
2020-03-09 10:31:57 +0530

    Add files via upload

---
+++ emi.c
@@ -0,0 +1,17 @@
+ #include<stdio.h>
+#include<math.h>
+void main()
+{
+int p,t;
+float emi,r,x,z,y;
+printf("enter the value of p,r,t\n");
+scanf("%d %f",&p,&r,&t);
+x=t*12;
+y=r/(100*12);
+z=pow((1+y),x);
+emi=(p*y*z)/(z-1);
+printf("z=%f",z);
+printf("y=%f",y);
+printf("x=%f",x);
+printf("emi=%f",emi);
+}
